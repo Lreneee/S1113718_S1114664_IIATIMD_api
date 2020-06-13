e79aac6a31337a2c363c25c29d33ba6634b5dcfa
Lrene <irenedekoning1@gmail.com>
2020-06-13 13:27:07 +0200

    Added user id + tips moestuin table

---
+++ IIATIMD-API/database/migrations/2020_06_13_112423_create_tips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('short_description', 200);
+            $table->string('long_description', 2000);
+            $table->string('img')->default("Geen afbeelding");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tips');
+    }
+}
