3e2d06225cf0828b81ed08c3d24f62b80ca8b6a7
Anne Sophie Huibregtsen <s1114664@student.hsleiden.nl>
2020-07-01 20:37:58 +0200

    pushen van meldingen werkt.

--- IIATIMD-API/app/Http/Controllers/Controller.php
+++ IIATIMD-API/app/Http/Controllers/Controller.php
@@ -2,12 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Kernel;
+use App\Token;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function test()
+    {
+     return view('test');
+    }
+
+    public function opdracht(Request $request)
+    {
+        $token = "";
+        $this->notification($token, request('opdracht'));
+        return redirect()->back();
+    }
+
+    public function token(Request $request) {
+        $token = Token::create($request->all());
+        return $token;
+    }
+
+    public function notification($token, $title)
+    {
+        $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+
+        $notification = [
+            'title' => $title,
+            'sound' => true,
+        ];
+
+        $extraNotificationData = ["message" => $notification,"moredata" =>'dd'];
+
+        $fcmNotification = [
+            //'registration_ids' => $tokenList, //multple token array
+            'to'        => $token, //single token
+            'notification' => $notification,
+            'data' => $extraNotificationData
+        ];
+
+        $headers = [
+            'Authorization: key=' . env('FCM_TOKEN'),
+            'Content-Type: application/json'
+        ];
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$fcmUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        dd($result);
+
+        return true;
+    }
+
 }
--- IIATIMD-API/routes/web.php
+++ IIATIMD-API/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', 'Controller@test');
+Route::post('/opdracht','Controller@opdracht')->name("opdracht");
+
 Route::get('/', function () {
     return view('welcome');
 });
